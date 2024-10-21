4012be71a2a2b5bde180b0e35acbe21d0f73fcce
Bhavesh Jain <jainbhavesh888@gmail.com>
2024-10-21 22:27:08 +0530

    Added admin component

--- client/src/AllRoutes/allRoutes.jsx
+++ client/src/AllRoutes/allRoutes.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Signup from "../Pages/Signup";
 import Bag from "../Pages/Bag";
 import AdminPrivateRoute from "../PrivateRoute/AdminPrivateRoute";
 import Cart from "../Pages/Cart";
-
+import AdminDashboard from "../Pages/AdminDashboard";
 
 const allRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/bags" element={<Bag/>}/>
-        <Route path="/cart" element={
-            <PrivateRoute>
-                <Cart/>
-            </PrivateRoute>
-            }/>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/bags" element={<Bag />} />
+      <Route
+        path="/cart"
+        element={
+          <PrivateRoute>
+            <Cart />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/admin_dashboard"
+        element={
+          <PrivateRoute>
+            < AdminDashboard/>
+          </PrivateRoute>
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default allRoutes
+export default allRoutes;
