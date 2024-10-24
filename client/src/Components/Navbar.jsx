b78a78d762ebf49c37384723e81d656749ce9740
Bhavesh Jain <jainbhavesh888@gmail.com>
2024-10-24 22:52:16 +0530

    Added some changes

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from "react"
-  const Navbar = () =>{
-return (
+import React from 'react';
+import { useState } from 'react';
+
+const Navbar = () =>{
+
+  return (
 
     <div className="px-1.5 py-1.5 text-white flex  justify-between bg-rose-500 border-2">
-    <div className ="w-12"> 
+      <div className="w-0.5"> 
+      <div className ="w-12"> 
         <img src="https://i.imgur.com/8SSRICl.png" className= "w-full m-auto"alt="img1"/>
       </div>
-      <div className=" px-[10px] text-white flex justify-evenly">
+      </div>
+      <div className=" w-0.5 px-[10px] text-white flex justify-evenly">
         <div>Cart</div>
-        <div>Wishlist</div>
         <div>Login</div>
       </div>
       
@@ -17,4 +21,3 @@ return (
 )
 };
 export default Navbar 
-
