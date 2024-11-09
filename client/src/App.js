4d8a0397887baa15c60660cb88470584f4df3286
Bhavesh Jain <jainbhavesh888@gmail.com>
2024-11-09 23:25:52 +0530

    added new change

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Signup from './Pages/Signup';
 import Home from './Pages/Home';
 import ProjectRoutes from './AllRoutes/ProjectRoutes';
 import Navbar from './Components/Navbar';
+import Footers from './Components/Footers';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <ProjectRoutes/>
+      <Footers/>
     </div>
   );
 }
